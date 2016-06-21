a3c537ee8c65016df4758e5c30b3fe08e59055aa
Slashthedragon <slashinme@gmail.com>
2016-06-21 19:33:53 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# audubon_practice
-1 player game to learn bird songs.
---
+++ app/src/main/java/first/denis/example/com/audubonpractice/MainActivity.java
@@ -0,0 +1,37 @@
+package first.denis.example.com.audubonpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//TODO public class Bird
+// Give it name, image and song. Is ID added automatically.
+
+//TODO Make an audio player.
+
+//TODO make a data base with the name, image and song
+
+//TODO Use a randomizer to select a bird and put it into the array for the display.
+
+//TODO Use a randomizer to select one of the birds in the display.
+
+//TODO play the selected bird when play button is clicked.
+
+//TODO select the bird associated to the display button which is clicked.
+
+//TODO Play the selected bird song.
+
+//TODO If the two selected birds are the same, change the border of the bird display mage, increment the counter
+
+//TODO  Remove the bird from the possible selected birds.
+
+//TODO
+
+
